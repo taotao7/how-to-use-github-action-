dfbb1cccc03de8b3614332b99124fa786a4558aa
xt01102058 <moca_tao7@foxmail.com>
2021-10-14 11:07:51 +0800

    feat: to add feat

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import Reveal from 'reveal.js'
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js'
+import RevealHighlight from 'reveal.js/plugin/highlight/highlight.esm.js'
 
 
 Reveal.initialize({
-  plugins: [Markdown],
+  plugins: [
+    Markdown,
+    RevealHighlight
+  ],
 });
